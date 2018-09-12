c749ea2ba9f647d99b1265c468786703246b7dad
Umair Qaisar <qaisaru@icloud.com>
2018-09-12 09:37:22 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,14 +1,9 @@
 def instantiate_new_array
-  new_array = array.new
+  new_array = Array.new
   return new_array
 end
 
 def array_with_two_elements
-  new_array= array.new(2)
+  new_array = Array.new(2)
   return new_array
-end
-
-def first_element(hello_sir)
-  my_first_element = hello_sir [0]
-  return my_first_element
-end
+end 
