fe1d220120b54b372ea7147ca22b3971133a003e
Umair Qaisar <qaisaru@icloud.com>
2018-09-12 09:44:04 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -13,7 +13,28 @@ def first_element (ed_de)
   return my_first_element
 end
 
-def third_element (de_ed)
-  my_third_element = de_ed [2]
+def third_element (ed_de)
+  my_third_element = ed_de [2]
   return my_third_element
 end
+
+def last_element (ed_de)
+  my_last_element = ed_de [-1]
+  return my_last_element
+end
+
+def first_element_with_array_methods(south_east_asia)
+  first_country = south_east_asia.first 
+  return first_country
+end
+
+def last_element_with_array_methods(south_east_asia)
+  last_country = south_east_asia.last 
+  return last_country
+end
+
+def length_of_array(programming_languages)
+  length = programming_languages.count 
+  return length 
+end
+
